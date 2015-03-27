6b134b1474171e8dddb111bb30aba6a9feb73907
Subha Shankar <sshankar@vmware.com>
2015-03-27 12:09:10 -0700

    Changing to for loop

--- system/killprocesses_linux.sh
+++ system/killprocesses_linux.sh
@@ -2,7 +2,9 @@
 #Script for killing a given processname
 
 PID=`ps -ef | grep $1 | grep -v grep | awk '{print$2}'`
-if [[ "" != "$PID" ]]; then
-	echo "killing $PID"
-	kill -9 $PID
-fi
+echo $PID
+for temp in $PID
+do
+        echo "killing $temp"
+        kill -9 $temp
+done
