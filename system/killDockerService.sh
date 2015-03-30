665977ace42c5fdcc1c8a027891a335698c87d07
Subha Shankar <sshankar@vmware.com>
2015-03-30 14:09:07 -0700

    Adding killing a docker service

---
+++ system/killDockerService.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# set -x
+#Script for killing a given process in docker container 
+#connect to cds variables with the deployment name 
+#
+CONTAINERID=`docker ps| grep $1 | grep -v grep | awk '{print $1}'`
+echo $CONTAIINERID
+for temp in $CONTAINERID
+do
+        echo "Stopping $temp"
+        docker stop 
+done
