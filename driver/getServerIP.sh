4a4cdf802fa9babafd5bf52e77315cdac646e067
Subha Shankar <sshankar@vmware.com>
2015-05-12 18:23:17 -0700

    Adding files to get host ip when service name is provided

---
+++ driver/getServerIP.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+if [ $# -eq 0 ]; then
+    echo "Usage: getServerIp.sh [service-name] "
+    exit
+fi
+
+echo "First parameter:" $1
+
+# Service name
+serviceName=$1
+
+cdsHost="192.168.20.5"
+cdsUser=root
+cdsPass=password
+
+
+#INT SHARD ENVT
+#cd /Users/sshankar/Downloads/cds-cli/darwin_amd64/
+#./cds svc show --all --compact --json > ./cdsSvcResult.json
+#./cds vms --json > ./cdsVmsJsonOutput.json
+
+#Any CDS Envt
+#cd /Users/sshankar/Documents/repositories/dbaas-release/dev/cds
+./sshpassBaseScript.sh $cdsHost $cdsUser $cdsPass "cds svc show --all --compact --json" > cdsSvcResult.json
+./sshpassBaseScript.sh $cdsHost $cdsUser $cdsPass "cds vms --json" > cdsVmsJsonOutput.json
+#vmId=`cat cdsSvcResult.json |jq '.[]|select (."name" == "$serviceName")|.vm.id'|tr -d '"'`
+#echo "VMID for $serviceName:"
+vmId=$(echo "cat cdsSvcResult.json |jq '.[]|select (."name" == \"$serviceName\")|.vm.id'|tr -d '\"'" | sh -fx)
+#echo $vmId
+#serviceId=`cat cdsSvcResult.json |jq '.[]|select (."name" == "$serviceName")|.id'|tr -d '\"'`
+#echo "SERVICEID for $serviceName:"
+serviceId=$(echo "cat cdsSvcResult.json |jq '.[]|select (."name" == \"$serviceName\")|.id'|tr -d '\"'" | sh -fx)
+#echo $serviceId
+#vmHost=`cat cdsVmsJsonOutput.json |jq '.[]|select (."id" == "433").nics|.[]|.ip'|tr -d '"'`
+echo "HOST ID FOR IAM:"
+vmHost=$(echo "cat cdsVmsJsonOutput.json |jq '.[]|select (."id" == \"$vmId\").nics|.[]|.ip'|tr -d '\"'" | sh -fx)
+echo $vmHost
---
+++ driver/sshpassBaseScript.sh
@@ -0,0 +1,2 @@
+i#!/bin/sh
+/usr/bin/sshpass -p $3 ssh $2@$1 $4
